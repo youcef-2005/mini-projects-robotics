0444a23554dcb321793f7989a21949a58f904f3d
youcef-2005 <hammacheyoucef2005@gmail.com>
2026-04-24 15:52:37 +0100

    feat: add subscriber node with logging

---
+++ mini_project_1/mini_project_1/subscriber.py
@@ -0,0 +1,26 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+class MinimalSubscriber(Node):
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'mini_project_topic',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info(f'Data Received: "{msg.data}"')
+
+def main(args=None):
+    rclpy.init(args=args)
+    minimal_subscriber = MinimalSubscriber()
+    rclpy.spin(minimal_subscriber)
+    minimal_subscriber.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
