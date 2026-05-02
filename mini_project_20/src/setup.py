86230a3abc8f7445661f4397f31580f457fe2221
youcef-2005 <hammacheyoucef2005@gmail.com>
2026-05-02 19:59:10 +0100

    Add setup.py for mini_project_20 package
    Initial setup configuration for the mini_project_20 package.

---
+++ mini_project_20/src/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'mini_project_20'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'numpy'],
+    zip_safe=True,
+    maintainer='Youcef',
+    maintainer_email='youcef@todo.todo',
+    description='AI Q-Learning Navigator Node',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'q_learning_agent = src.q_learning_agent:main',
+        ],
+    },
+)
