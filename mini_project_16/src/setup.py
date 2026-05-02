2db38fe4e96ad0a9532f4b2ae51d7fb2ab2da8c6
youcef-2005 <hammacheyoucef2005@gmail.com>
2026-05-02 20:10:56 +0100

    Create setup.py for package configuration
    Set up package configuration for mini_project_16 with dependencies and entry points.

---
+++ mini_project_16/src/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'mini_project_16'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools', 'numpy'],
+    zip_safe=True,
+    maintainer='Youcef',
+    maintainer_email='youcef@todo.todo',
+    description='Artificial Potential Fields Navigation Node',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'potential_field_nav = src.potential_field_navigation:main',
+        ],
+    },
+)
