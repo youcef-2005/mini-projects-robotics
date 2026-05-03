dcce7215982c169145ce56cad361c9bd41fdb8bb
youcef-2005 <hammacheyoucef2005@gmail.com>
2026-05-03 20:27:59 +0100

    Add setup.py for mini_project_06 package

---
+++ mini_project_1/mini_project_1/mini_project_6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'mini_project_06'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Youcef',
+    maintainer_email='youcef@todo.todo',
+    description='Pub/Sub Communication Nodes',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'node_publisher = src.node_publisher:main',
+            'node_subscriber = src.node_subscriber:main',
+        ],
+    },
+)
